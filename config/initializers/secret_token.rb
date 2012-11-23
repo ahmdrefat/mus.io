3f46cb4169ec9fd64edaf7924152cd3c81ef583a
ahmd refat twfik <ahmd.refat.twfik@gmail.com>
2012-11-23 18:58:28 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusIo::Application.config.secret_token = '74f046caca0b836200d7a79e9d1178a6d2ccfa6c4f26a53d08d1936fb252e00f8b34b2259f96a50516cb4f5680bc48419a8912910a36ae8f709bf1cde346a9d3'
